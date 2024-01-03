e5dcf17f0886df86f0281901e8561a35e5263a1c
Pedro Ulhoa <pedroulhoa@live.com>
2024-01-03 12:55:17 -0300

    multiple results in function

--- tour/functions.go
+++ tour/functions.go
@@ -8,9 +8,15 @@ func add(x int, y int) int {
 	return x + y
 }
 
-// shortened
+// A function can return multiple results
+func swap(textA string, textB string) (string, string) {
+	return textA, textB
+}
+
+// shortened define type
 // func add(x, y int)
 
 func main() {
 	fmt.Println(add(40, 40))
+	fmt.Println(swap("Hello", "World"))
 }
